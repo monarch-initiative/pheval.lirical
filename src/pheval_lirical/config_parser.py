c772113627d2b41387f82cf97f37405e28f3793e
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-02-21 14:32:54 +0000

    updating config

---
+++ src/pheval_lirical/config_parser.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import yaml
+from serde import serde
+from serde.yaml import from_yaml
+
+
+@serde
+@dataclass
+class ExomiserLiricalExomiserConfigs:
+    path_to_exomiser_data_directory: Path
+
+
+@serde
+@dataclass
+class LiricalConfigRun:
+    environment: str
+    version: str
+    path_to_lirical_software_directory: Path
+    exomiser_configurations: ExomiserLiricalExomiserConfigs
+
+
+@serde
+@dataclass
+class LiricalPostProcess:
+    score_order: str
+
+
+@serde
+@dataclass
+class LiricalConfig:
+    run: LiricalConfigRun
+    post_process: LiricalPostProcess
+
+
+def parse_lirical_config(config_path: Path) -> LiricalConfig:
+    """Parse the config file."""
+    with open(config_path, "r") as config_file:
+        config = yaml.safe_load(config_file)
+    config_file.close()
+    return from_yaml(LiricalConfig, yaml.dump(config))
