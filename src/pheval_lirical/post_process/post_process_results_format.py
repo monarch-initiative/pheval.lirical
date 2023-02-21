7460f0880794b5de289213ce130ca9e8cbc38904
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-02-21 15:47:25 +0000

    fixing config variable name

--- src/pheval_lirical/post_process/post_process_results_format.py
+++ src/pheval_lirical/post_process/post_process_results_format.py
@@ -1,12 +1,17 @@
 import re
 from pathlib import Path
-from pheval.post_processing.post_processing import (
-    PhEvalVariantResult, RankedPhEvalVariantResult, create_pheval_result,
-    write_pheval_gene_result, write_pheval_variant_result, PhEvalGeneResult, RankedPhEvalGeneResult
-)
 
-import pandas as pd
 import click
+import pandas as pd
+from pheval.post_processing.post_processing import (
+    PhEvalGeneResult,
+    PhEvalVariantResult,
+    RankedPhEvalGeneResult,
+    RankedPhEvalVariantResult,
+    create_pheval_result,
+    write_pheval_gene_result,
+    write_pheval_variant_result,
+)
 from pheval.utils.file_utils import files_with_suffix
 from pheval.utils.phenopacket_utils import GeneIdentifierUpdater, create_hgnc_dict
 
@@ -20,7 +25,9 @@ def calculate_end(variant_start: int, variant_ref: str) -> int:
     return int(variant_start) + int(len(variant_ref)) - 1
 
 
-def obtain_gene_symbol_from_identifier(gene_identifier: str, hgnc_data: dict, identifier: str) -> str:
+def obtain_gene_symbol_from_identifier(
+    gene_identifier: str, hgnc_data: dict, identifier: str
+) -> str:
     # TODO move to PhEval GeneIdentifierupdator
     for symbol, data in hgnc_data.items():
         if gene_identifier == data[identifier]:
@@ -28,7 +35,12 @@ def obtain_gene_symbol_from_identifier(gene_identifier: str, hgnc_data: dict, id
 
 
 class PhEvalGeneResultFromLirical:
-    def __init__(self, lirical_result: pd.DataFrame, hgnc_data: dict, gene_identifier_updator: GeneIdentifierUpdater):
+    def __init__(
+        self,
+        lirical_result: pd.DataFrame,
+        hgnc_data: dict,
+        gene_identifier_updator: GeneIdentifierUpdater,
+    ):
         self.lirical_result = lirical_result
         self.hgnc_data = hgnc_data
         self.gene_identifier_updator = gene_identifier_updator
@@ -39,7 +51,9 @@ class PhEvalGeneResultFromLirical:
 
     def obtain_gene_symbol(self, result: pd.Series):
         gene_identifier = self.obtain_lirical_gene_identifier(result)
-        return obtain_gene_symbol_from_identifier(gene_identifier.split(":")[1], self.hgnc_data, "entrez_id")
+        return obtain_gene_symbol_from_identifier(
+            gene_identifier.split(":")[1], self.hgnc_data, "entrez_id"
+        )
 
     def obtain_gene_identifier(self, result: pd.Series):
         gene_symbol = self.obtain_gene_symbol(result)
@@ -52,16 +66,20 @@ class PhEvalGeneResultFromLirical:
     def extract_pheval_requirements(self):
         simplified_gene_results = []
         for _index, result in self.lirical_result.iterrows():
-            simplified_gene_results.append(PhEvalGeneResult(gene_symbol=self.obtain_gene_symbol(result),
-                                                            gene_identifier=self.obtain_gene_identifier(result),
-                                                            score=self.obtain_score(result)))
+            simplified_gene_results.append(
+                PhEvalGeneResult(
+                    gene_symbol=self.obtain_gene_symbol(result),
+                    gene_identifier=self.obtain_gene_identifier(result),
+                    score=self.obtain_score(result),
+                )
+            )
         return simplified_gene_results
 
 
 class PhEvalVariantResultFromLirical:
     def __init__(self, lirical_result: pd.DataFrame):
         self.lirical_result = lirical_result
-        self.reg = re.compile(r'(?P<numbers>\d*)(?P<rest>.*)')
+        self.reg = re.compile(r"(?P<numbers>\d*)(?P<rest>.*)")
 
     @staticmethod
     def obtain_score(lirical_result_entry: pd.Series) -> float:
@@ -73,7 +91,7 @@ class PhEvalVariantResultFromLirical:
 
     def split_variants(self, lirical_result_entry):
         variants = self.obtain_variants(lirical_result_entry)
-        return [x.strip() for x in variants.split(';')]
+        return [x.strip() for x in variants.split(";")]
 
     @staticmethod
     def get_variant_string(variant: str):
@@ -85,11 +103,11 @@ class PhEvalVariantResultFromLirical:
 
     def obtain_start(self, variant_str: str):
         variant = self.get_variant_string(variant_str)
-        return self.reg.search(variant.split(":")[1]).group('numbers')
+        return self.reg.search(variant.split(":")[1]).group("numbers")
 
     def obtain_ref(self, variant_str: str):
         variant = self.get_variant_string(variant_str)
-        return self.reg.search(variant.split(":")[1]).group('rest').split(">")[0]
+        return self.reg.search(variant.split(":")[1]).group("rest").split(">")[0]
 
     def obtain_end(self, variant_str: str):
         variant = self.get_variant_string(variant_str)
@@ -97,7 +115,7 @@ class PhEvalVariantResultFromLirical:
 
     def obtain_alt(self, variant_str: str):
         variant = self.get_variant_string(variant_str)
-        return self.reg.search(variant.split(":")[1]).group('rest').split(">")[1]
+        return self.reg.search(variant.split(":")[1]).group("rest").split(">")[1]
 
     def extract_pheval_requirements(self):
         simplified_variant_results = []
@@ -106,31 +124,35 @@ class PhEvalVariantResultFromLirical:
             for variant_result in variant_results:
                 if variant_result != "":
                     variant = self.get_variant_string(variant_result)
-                    simplified_variant_results.append(PhEvalVariantResult(chromosome=self.obtain_chromosome(variant),
-                                                                          start=self.obtain_start(variant),
-                                                                          end=self.obtain_end(variant),
-                                                                          ref=self.obtain_ref(variant),
-                                                                          alt=self.obtain_alt(variant),
-                                                                          score=self.obtain_score(result)))
+                    simplified_variant_results.append(
+                        PhEvalVariantResult(
+                            chromosome=self.obtain_chromosome(variant),
+                            start=self.obtain_start(variant),
+                            end=self.obtain_end(variant),
+                            ref=self.obtain_ref(variant),
+                            alt=self.obtain_alt(variant),
+                            score=self.obtain_score(result),
+                        )
+                    )
         return simplified_variant_results
 
 
 def create_variant_gene_result_from_lirical(
-        lirical_tsv_result: pd.DataFrame,
-        score_order: str,
+    lirical_tsv_result: pd.DataFrame,
+    sort_order: str,
 ) -> [RankedPhEvalVariantResult]:
     """Create ranked PhEval variant result from LIRICAL tsv."""
     pheval_variant_result = PhEvalVariantResultFromLirical(
         lirical_tsv_result
     ).extract_pheval_requirements()
-    return create_pheval_result(pheval_variant_result, score_order)
+    return create_pheval_result(pheval_variant_result, sort_order)
 
 
 def create_pheval_gene_result_from_lirical(
-        lirical_tsv_result: pd.DataFrame,
-        gene_identifier_updator: GeneIdentifierUpdater,
-        hgnc_data: dict,
-        sort_order: str
+    lirical_tsv_result: pd.DataFrame,
+    gene_identifier_updator: GeneIdentifierUpdater,
+    hgnc_data: dict,
+    sort_order: str,
 ) -> [RankedPhEvalGeneResult]:
     """Create ranked PhEval gene result from Phen2Gene tsv."""
     pheval_gene_result = PhEvalGeneResultFromLirical(
@@ -139,9 +161,7 @@ def create_pheval_gene_result_from_lirical(
     return create_pheval_result(pheval_gene_result, sort_order)
 
 
-def create_standardised_results(
-        results_dir: Path, output_dir: Path, sort_order: str
-) -> None:
+def create_standardised_results(results_dir: Path, output_dir: Path, sort_order: str) -> None:
     """Write standardised gene and variant results from LIRICAL tsv output."""
     output_dir.joinpath("pheval_gene_results/").mkdir(exist_ok=True, parents=True)
     output_dir.joinpath("pheval_variant_results/").mkdir(exist_ok=True, parents=True)
@@ -153,16 +173,23 @@ def create_standardised_results(
             lirical_result, gene_identifier_updator, hgnc_data, sort_order
         )
         write_pheval_gene_result(pheval_gene_result, output_dir, result)
-        pheval_variant_result = create_variant_gene_result_from_lirical(
-            lirical_result, sort_order
-        )
+        pheval_variant_result = create_variant_gene_result_from_lirical(lirical_result, sort_order)
         write_pheval_variant_result(pheval_variant_result, output_dir, result)
 
 
 @click.command("post-process")
-@click.option("--lirical-file", "-f", required=True, help="Path to Lirical results file.", type=Path)
+@click.option(
+    "--lirical-file", "-f", required=True, help="Path to Lirical results file.", type=Path
+)
 @click.option("--output-dir", "-o", required=True, help="Path to output directory.", type=Path)
-@click.option("--score-order", "-s", required=True, help="score order.", type=click.Choice(["ascending", "descending"]),
-              default="descending", show_default=True)
-def post_process(lirical_file: Path, output_dir, score_order):
-    create_standardised_results(lirical_file, output_dir, score_order)
+@click.option(
+    "--sort-order",
+    "-s",
+    required=True,
+    help="sort order.",
+    type=click.Choice(["ascending", "descending"]),
+    default="descending",
+    show_default=True,
+)
+def post_process(lirical_file: Path, output_dir, sort_order):
+    create_standardised_results(lirical_file, output_dir, sort_order)
