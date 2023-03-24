9cc0f9fd7eb21a581a8ca2674a85279a6ff6209a
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-24 16:08:30 +0000

    removing manual path setting

--- src/pheval_lirical/post_process/post_process.py
+++ src/pheval_lirical/post_process/post_process.py
@@ -1,21 +1,15 @@
-import os
 from pathlib import Path
 
 from pheval_lirical.config_parser import LiricalConfig
 from pheval_lirical.post_process.post_process_results_format import create_standardised_results
 
 
-def post_process_results_format(
-    input_dir: Path, testdata_dir: Path, output_dir: Path, config: LiricalConfig
-):
+def post_process_results_format(raw_results_dir: Path, output_dir: Path, config: LiricalConfig):
     """Create pheval gene and variant result from LIRICAL tsv output."""
     print("...creating pheval results format...")
-    run_output_dir = Path(output_dir).joinpath(
-        f"lirical_{config.run.version}_{Path(input_dir).name}{os.sep}{os.path.basename(testdata_dir)}_results"
-    )
     create_standardised_results(
-        results_dir=Path(run_output_dir).joinpath("lirical_results"),
-        output_dir=run_output_dir,
+        raw_results_dir=raw_results_dir,
+        output_dir=output_dir,
         sort_order=config.post_process.sort_order,
     )
     print("done")
