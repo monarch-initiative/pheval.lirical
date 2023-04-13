048512d8fa882e53acca2d4972539bdaa2094f14
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-13 12:28:54 +0100

    adding arguments for Exomiser data

--- src/pheval_lirical/prepare/prepare_phenopacket_commands.py
+++ src/pheval_lirical/prepare/prepare_phenopacket_commands.py
@@ -1,17 +1,16 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-from pheval.utils.phenopacket_utils import PhenopacketUtil
-from phenopackets import Phenopacket
-
 
 @dataclass
 class LiricalPhenopacketCommandLineArguments:
-    lirical_jar_path: Path
+    lirical_jar_file: Path
     phenopacket_path: Path
     vcf_file_path: Path
     assembly: str
     lirical_data: Path
-    exomiser_data_path: Path
+    exomiser_data: Path
     output_dir: Path
     output_prefix: str
+    exomiser_hg19_data_path: Path = None
+    exomiser_hg38_data_path: Path = None
